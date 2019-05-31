e56fda500ce14ec336e99cbff838247ac4d7ed93
Christy Kusuma <christykusuma@Christys-MacBook-Pro.local>
2019-05-31 20:44:44 +0700

    added typescript to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,11 @@
 import { configure, setAddon, addDecorator } from '@storybook/react'
 import JSXAddon from 'storybook-addon-jsx'
+import { withKnobs } from '@storybook/addon-knobs/react';
 import { withInfo } from '@storybook/addon-info'
 
 setAddon(JSXAddon);
 addDecorator(withInfo);
+addDecorator(withKnobs);
 const req = require.context('../src', true, /.stories.js$/)
 
 function loadStories() {
--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { Button } from './Button'
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Button } from './Button';
+import { color } from '@storybook/addon-knobs/react';
+import { text, boolean } from '@storybook/addon-knobs/react';
 
 // add changed to addWithJSX
 storiesOf('Button', module)
@@ -16,8 +18,13 @@ storiesOf('Button', module)
         },
     })
     .addWithJSX('with background', () => ( 
-        <Button bg="palegoldenrod">Hello World</Button>
+        <Button bg={text('bg', 'palegoldenrod')}>Hello world2</Button>
     ))
     .addWithJSX('with background 2', () => (
-        <Button bg="green">Hello World</Button>
+        <Button bg={color('bg', 'green', 'group1')}>Hello world2</Button>
     ))
+    .addWithJSX('with Typescript', () => (
+        <Button disabled={boolean('disabled', false)}>
+            {text('text', 'Hello world')}
+        </Button>
+    ));
